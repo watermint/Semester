59cbbc525c4da76fb5cc380075654cef623df6c8
Takayuki Okazaki <takayuki.okazaki@gmail.com>
2013-11-09 00:41:52 +0900

    add loop

--- libs/etude-chatwork/src/main/scala/etude/chatwork/Stenographer.scala
+++ libs/etude-chatwork/src/main/scala/etude/chatwork/Stenographer.scala
@@ -1,30 +1,49 @@
 package etude.chatwork
 
-import etude.qos.Throttle
 import scala.collection.mutable
+import java.time.Instant
 
 /**
  *
  */
 case class Stenographer(session: Session) {
-  def allMessages(roomMeta: RoomMeta): List[Message] = {
+  def loop(roomMeta: RoomMeta, after: Option[Instant], f: (List[Message]) => Boolean): Unit = {
     session.room(roomMeta) match {
-      case None => List()
+      case None =>
       case Some(room) => {
-        val allMessages = mutable.ListBuffer[Message]()
-
-        allMessages ++= room.messages
-
-        var currentMessages = room.messages
-
-        while (currentMessages.size > 0) {
-          val lastMessage = currentMessages.minBy(_.timestamp)
-          currentMessages = session.messages(lastMessage)
-          allMessages ++= currentMessages
+        var currentMessages = room.messages.toList
+        var oldestMessage = currentMessages.minBy(_.timestamp)
+        val afterInstant = after match {
+          case Some(a) => a
+          case None => Instant.EPOCH
         }
 
-        allMessages.toList
+        while (currentMessages.size > 0
+          && f(currentMessages)
+          && afterInstant.isBefore(oldestMessage.timestamp)) {
+
+          oldestMessage = currentMessages.minBy(_.timestamp)
+          currentMessages = session.messages(oldestMessage)
+        }
       }
     }
   }
+
+  def loop(roomMeta: RoomMeta, f: (List[Message]) => Boolean): Unit = {
+    loop(roomMeta, None, f)
+  }
+
+  def allMessages(roomMeta: RoomMeta, after: Option[Instant]): List[Message] = {
+    val allMessages = mutable.ListBuffer[Message]()
+
+    loop(roomMeta, after, {
+      (list) =>
+        allMessages ++= list
+        true
+    })
+
+    allMessages.toList
+  }
+
+  def allMessages(roomMeta: RoomMeta): List[Message] = allMessages(roomMeta, None)
 }
