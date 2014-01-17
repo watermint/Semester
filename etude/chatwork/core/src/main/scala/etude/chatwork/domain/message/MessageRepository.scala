50e489524031f53a61feffe673e852121092c1a5
Takayuki Okazaki <takayuki.okazaki@gree.net>
2014-01-17 18:41:13 +0900

    fix syntax

--- etude/chatwork/core/src/main/scala/etude/chatwork/domain/message/MessageRepository.scala
+++ etude/chatwork/core/src/main/scala/etude/chatwork/domain/message/MessageRepository.scala
@@ -3,7 +3,7 @@ package etude.chatwork.domain.message
 import scala.language.higherKinds
 import etude.foundation.domain.lifecycle.EntityIOContext
 
-trait MessageRepository[M[+A]]cd
+trait MessageRepository[M[+A]]
   extends MessageReader[M] {
 
   def markAsRead(message: MessageId)(implicit context: EntityIOContext[M]): M[MessageId]
