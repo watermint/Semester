99e5dce414489ce9bcc2927456ad5383c9844a05
Takayuki Okazaki <takayuki.okazaki@gmail.com>
2014-06-20 16:21:50 +0900

    load room list

--- etude/app/arrabbiata/src/main/scala/etude/app/arrabbiata/controller/message/room/LoadRoomList.scala
+++ etude/app/arrabbiata/src/main/scala/etude/app/arrabbiata/controller/message/room/LoadRoomList.scala
@@ -1,14 +1,25 @@
 package etude.app.arrabbiata.controller.message.room
 
 import etude.app.arrabbiata.controller.message.{CallbackMessage, MessageWithSession}
-import etude.app.arrabbiata.state.Session
+import etude.app.arrabbiata.state.{Rooms, Session}
+import etude.app.arrabbiata.ui.UIActor
 import etude.app.arrabbiata.ui.message.UIMessage
+import etude.messaging.chatwork.domain.lifecycle.room.{AsyncParticipantRepository, AsyncRoomRepository}
 
 case class LoadRoomList(uiMessage: UIMessage)
   extends MessageWithSession
   with CallbackMessage {
 
   def perform(session: Session): Unit = {
+    implicit val ioContext = session.ioContext
+    implicit val execContext = session.ioContext.executor
+    val roomRepo = AsyncRoomRepository.ofContext(ioContext)
 
+    roomRepo.rooms() map {
+      r =>
+        Rooms.rooms.clear()
+        Rooms.rooms.appendAll(r)
+        UIActor.ui ! uiMessage
+    }
   }
 }
--- etude/app/arrabbiata/src/main/scala/etude/app/arrabbiata/state/Rooms.scala
+++ etude/app/arrabbiata/src/main/scala/etude/app/arrabbiata/state/Rooms.scala
@@ -4,8 +4,6 @@ import etude.messaging.chatwork.domain.model.room.{Participant, Room}
 
 import scala.collection.mutable.ListBuffer
 
-object Room {
+object Rooms {
   val rooms: ListBuffer[Room] = new ListBuffer[Room]
-
-  val participants: ListBuffer[Participant] = new ListBuffer[Participant]
 }
