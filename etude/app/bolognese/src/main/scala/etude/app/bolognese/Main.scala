6313950a92f8a270c85adc2ee6734618b9cd4ff1
Takayuki Okazaki <takayuki.okazaki@gmail.com>
2014-07-08 06:53:55 +0900

    spray with future operation

--- etude/app/bolognese/src/main/scala/etude/app/bolognese/Main.scala
+++ etude/app/bolognese/src/main/scala/etude/app/bolognese/Main.scala
@@ -1,15 +1,38 @@
 package etude.app.bolognese
 
+import java.util.concurrent.{Executors, ExecutorService}
+
 import akka.actor.ActorSystem
 import spray.http.HttpCookie
 import spray.routing.SimpleRoutingApp
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 object Main
   extends App
   with SimpleRoutingApp
   with SecureConfiguration {
 
+  def keyStoreResource: String = "/bolognese.jks"
+
+  def keyStorePassword: String = "semester"
+
+  // for processing future operation within onComplete
+  val executorsPool: ExecutorService = Executors.newCachedThreadPool()
+
+  implicit val executors = ExecutionContext.fromExecutorService(executorsPool)
+
   implicit val system = ActorSystem("Bolognese")
+
+  def heavyOperation(name: String): Future[String] = {
+    Thread.sleep(10000)
+    if (name.startsWith("a")) {
+      Future.successful(s"10 seconds: $name")
+    } else {
+      Future.failed(new IllegalArgumentException("name not starts with 'a'"))
+    }
+  }
 
   def hello(name: String) = {
     get {
@@ -20,15 +43,27 @@ object Main
               {name}{c.content}
             </h1>
           }
-      } ~ complete {
-        "no cookie"
+      } ~ onComplete(heavyOperation(name)) {
+        case Success(s) => complete {
+          s
+        }
+        case Failure(f) => complete {
+          s"error $f"
+        }
       }
     }
   }
 
   def hello2() = {
     post {
-      setCookie(new HttpCookie("hello", "world")) {
+      setCookie(
+        new HttpCookie(
+          name = "hello",
+          content = "world",
+          secure = true,
+          httpOnly = true
+        )
+      ) {
         complete {
           <h1>POSTing</h1>
         }
--- etude/app/bolognese/src/main/scala/etude/app/bolognese/SecureConfiguration.scala
+++ etude/app/bolognese/src/main/scala/etude/app/bolognese/SecureConfiguration.scala
@@ -6,17 +6,19 @@ import javax.net.ssl.{TrustManagerFactory, KeyManagerFactory, SSLContext}
 import spray.io.ServerSSLEngineProvider
 
 trait SecureConfiguration {
+  def keyStoreResource: String
+
+  def keyStorePassword: String
+
   implicit def sslContext: SSLContext = {
-    val keyStoreResource = "/bolognese.jks"
-    val password = "semester"
 
     val keyStore = KeyStore.getInstance("jks")
     val keyManagerFactory = KeyManagerFactory.getInstance("SunX509")
     val trustManagerFactory = TrustManagerFactory.getInstance("SunX509")
     val context = SSLContext.getInstance("TLS")
 
-    keyStore.load(getClass.getResourceAsStream(keyStoreResource), password.toCharArray)
-    keyManagerFactory.init(keyStore, password.toCharArray)
+    keyStore.load(getClass.getResourceAsStream(keyStoreResource), keyStorePassword.toCharArray)
+    keyManagerFactory.init(keyStore, keyStorePassword.toCharArray)
     trustManagerFactory.init(keyStore)
     context.init(keyManagerFactory.getKeyManagers, trustManagerFactory.getTrustManagers, new SecureRandom)
     context
@@ -24,9 +26,11 @@ trait SecureConfiguration {
 
   implicit val sslEngine = ServerSSLEngineProvider {
     engine =>
-      // TLS_RSA_WITH_AES_256_CBC_SHA
+      // TLS_RSA_WITH_AES_256_CBC_SHA requires Java Cryptography Extension (JCE) Unlimited Strength Jurisdiction Policy Files
+      // @see http://www.oracle.com/technetwork/java/javase/downloads/jce8-download-2133166.html
+
       engine.setEnabledCipherSuites(Array("TLS_RSA_WITH_AES_128_CBC_SHA"))
-      engine.setEnabledProtocols(Array("SSLv3", "TLSv1"))
+      engine.setEnabledProtocols(Array("SSLv3", "TLSv1.2"))
       engine
   }
 }
