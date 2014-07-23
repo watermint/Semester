2eecd152025dfdf159c8965d32220f05a58927b6
Takayuki Okazaki <takayuki.okazaki@gmail.com>
2014-07-23 10:47:44 +0900

    remove unused class

--- etude-epice-http/src/main/scala/etude/epice/http/Resource.scala
+++
@@ -1,9 +0,0 @@
-package etude.epice.http
-
-import scala.util.Try
-
-case class Resource(uri: URIContainer) {
-  def get: Try[Response] = SyncClient().get(uri)
-
-  def post(formData: Map[String, String] = Map.empty): Try[Response] = SyncClient().post(uri, formData)
-}
--- etude-epice-http/src/main/scala/etude/epice/http/package.scala
+++ etude-epice-http/src/main/scala/etude/epice/http/package.scala
@@ -1,12 +1,8 @@
 package etude.epice
 
-import java.net.{URI, URL}
+import java.net.URI
 
 package object http {
-  implicit def uriToResource(uri: URI): Resource = Resource(uri)
-
-  implicit def urlToResource(url: URL): Resource = Resource(url.toURI)
-
   implicit def uriToUriContainer(uri: URI): URIContainer = URIContainer(uri)
 
   implicit def uriContainerToUri(uriContainer: URIContainer): URI = uriContainer.uri
