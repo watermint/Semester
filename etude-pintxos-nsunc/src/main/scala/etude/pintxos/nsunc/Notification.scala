f7cd4101d234f0a60b5e9ee1993de27d0a9e4c00
Takayuki Okazaki <takayuki.okazaki@gmail.com>
2014-08-07 17:12:17 +0900

    update docs

--- etude-pintxos-nsunc/src/main/scala/etude/pintxos/nsunc/Notification.scala
+++ etude-pintxos-nsunc/src/main/scala/etude/pintxos/nsunc/Notification.scala
@@ -6,17 +6,30 @@ object Notification {
   val center: ID = Foundation.invoke(Foundation.getObjcClass("NSUserNotificationCenter"),
     "defaultUserNotificationCenter")
 
-  def notify(title: String, description: String) = {
+  private def escapeText(text: String): String = {
+    // Original code uses StringUtil#stripHtml which equivalent like below.
+    //    text.replaceAll("<br/?>", "\n\n").replaceAll("<(.|\n)*?>", "").replace("%", "%%")
+    text.replace("%", "%%")
+  }
+
+  /**
+   * @param title title.
+   * @param description description.
+   * @see @see https://developer.apple.com/library/mac/documentation/Foundation/Reference/NSUserNotification_Class/Reference/Reference.html#//apple_ref/doc/uid/TP40012259
+   */
+  def notify(title: String, description: String): Unit = {
     val notification: ID = Foundation.invoke(Foundation.getObjcClass("NSUserNotification"), "new")
 
-
-    Foundation.invoke(notification, "setTitle:", Foundation.nsString(title.replace("%", "%%")))
-    Foundation.invoke(notification, "setInformativeText:", Foundation.nsString(description.replace("%", "%%")))
+    Foundation.invoke(notification, "setTitle:", Foundation.nsString(escapeText(title)))
+    Foundation.invoke(notification, "setInformativeText:", Foundation.nsString(escapeText(description)))
 
     Foundation.invoke(center, "deliverNotification:", notification)
   }
 
-  def cleanup() = {
+  /**
+   * Clean up delivered notifications.
+   */
+  def cleanup(): Unit = {
     Foundation.invoke(center, "removeAllDeliveredNotifications")
   }
 }
