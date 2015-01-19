486588474ac4b9e3f9f3ecb38834a00aacb992d6
Takayuki Okazaki <takayuki.okazaki@gmail.com>
2015-01-19 11:19:16 +0900

    remove migrated methods

--- etude-vino-chatwork/src/main/scala/etude/vino/chatwork/domain/model/RoomChunk.scala
+++ etude-vino-chatwork/src/main/scala/etude/vino/chatwork/domain/model/RoomChunk.scala
@@ -2,33 +2,10 @@ package etude.vino.chatwork.domain.model
 
 import etude.manieres.domain.model.Entity
 import etude.pintxos.chatwork.domain.model.room.RoomId
-import org.json4s.JsonDSL._
-import org.json4s._
-
-
 
 case class RoomChunk(roomId: RoomId,
                      chunks: Seq[Chunk]) extends Entity[RoomId] {
 
   val identity: RoomId = roomId
-
-  def toJSON: JValue = {
-    ("roomId" -> roomId.value) ~
-      ("chunks" -> chunks.map(_.toJSON))
-  }
 }
 
-object RoomChunk {
-  def fromJSON(json: JValue): RoomChunk = {
-    (for {
-      JObject(o) <- json
-      JField("roomId", JInt(roomId)) <- o
-      JField("chunks", JArray(chunks)) <- o
-    } yield {
-      RoomChunk(
-        RoomId(roomId),
-        Chunk.ofChunks(chunks)
-      )
-    }).last
-  }
-}
