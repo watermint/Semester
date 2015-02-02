57f121941dcf6cd790a01fb69aef1a6a852ffeac
Takayuki Okazaki <takayuki.okazaki@gmail.com>
2015-02-03 01:19:05 +0900

    fix #4

--- etude-vino-chatwork/src/main/scala/etude/vino/chatwork/domain/state/Rooms.scala
+++ etude-vino-chatwork/src/main/scala/etude/vino/chatwork/domain/state/Rooms.scala
@@ -3,6 +3,7 @@ package etude.vino.chatwork.domain.state
 import akka.actor.{Actor, Props}
 import etude.pintxos.chatwork.domain.model.room.{Room, RoomId}
 import etude.pintxos.chatwork.domain.service.v0.response.InitLoadResponse
+import etude.vino.chatwork.ui.Main.ApplicationReady
 import etude.vino.chatwork.ui.UI
 import etude.vino.chatwork.ui.pane.RoomListPane.RoomListUpdate
 
@@ -19,6 +20,7 @@ class Rooms extends Actor {
         room =>
           self ! room
       }
+      UI.ref ! ApplicationReady()
 
     case r: Room =>
       Rooms.avatar.updateAvatar(r.roomId, r.avatar)
--- etude-vino-chatwork/src/main/scala/etude/vino/chatwork/ui/Main.scala
+++ etude-vino-chatwork/src/main/scala/etude/vino/chatwork/ui/Main.scala
@@ -5,48 +5,63 @@ import etude.vino.chatwork.ui.pane.{RoomListPane, ApplicationLogPane, MessageLis
 import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
+import scalafx.geometry.Pos
 import scalafx.scene.Scene
-import scalafx.scene.control.{Tab, TabPane}
-import scalafx.scene.layout.{Priority, HBox, BorderPane}
+import scalafx.scene.control.{ProgressIndicator, Tab, TabPane}
+import scalafx.scene.layout.{FlowPane, Priority, HBox, BorderPane}
 
 object Main extends JFXApp {
   UI.ref ! "startup"
+
+  case class ApplicationReady() extends UIMessage {
+    def perform(): Unit = {
+      rootPane.center = mainTabPane
+    }
+  }
+
+  val mainTabPane = new TabPane() {
+    tabs = Seq(
+      new Tab() {
+        text = "Chat Rooms"
+        closable = false
+        content = new HBox {
+          spacing = UIStyles.spacingWidth
+          hgrow = Priority.Always
+          vgrow = Priority.Always
+          children = Seq(
+            RoomListPane.roomListView,
+            MessageListPane.timelineOfRoom
+          )
+        }
+      },
+      new Tab() {
+        text = "Timeline"
+        closable = false
+        content = MessageListPane.messageTimeline
+      },
+      new Tab() {
+        text = "To/ReplyTo Me"
+        closable = false
+        content = MessageListPane.toMeMessageList
+      },
+      new Tab() {
+        text = "Application log"
+        content = ApplicationLogPane.applicationLog
+        closable = false
+      }
+    )
+  }
 
   val rootPane = new BorderPane {
 
     padding = UIStyles.paddingInsets
 
-    center = new TabPane() {
-      tabs = Seq(
-        new Tab() {
-          text = "Chat Rooms"
-          closable = false
-          content = new HBox {
-            spacing = UIStyles.spacingWidth
-            hgrow = Priority.Always
-            vgrow = Priority.Always
-            children = Seq(
-              RoomListPane.roomListView,
-              MessageListPane.timelineOfRoom
-            )
-          }
-        },
-        new Tab() {
-          text = "Timeline"
-          closable = false
-          content = MessageListPane.messageTimeline
-        },
-        new Tab() {
-          text = "To/ReplyTo Me"
-          closable = false
-          content = MessageListPane.toMeMessageList
-        },
-        new Tab() {
-          text = "Application log"
-          content = ApplicationLogPane.applicationLog
-          closable = false
-        }
-      )
+    center = new HBox {
+      alignment = Pos.Center
+      children = new ProgressIndicator {
+        prefWidth = 30
+        prefHeight = 30
+      }
     }
   }
 
